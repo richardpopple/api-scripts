cb8b80332ef0c8f90b9bd2349337f365308371be
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-01-05 16:38:34 +0000

    Functionality now present to add the dps to users whom need it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,61 @@
 import gateways.NomisUserRoleApi
-import gateways.RestrictedPatientsApi
-import fileaccess.PerOffenderFileOutput
-import fileaccess.SuccessfulOffenderMigrations
-import fileaccess.SynchronisedSummaryOutput
-import rpmigration.MigrateOffender
-import rpmigration.MigrationRequestEvent
-import rpmigration.Migrations
-import spreadsheetaccess.SpreadsheetReader
+import gateways.PrisonApi
 
 fun main() {
-    
-    val usernamesWithRole302InMdi = nomisUserRoleApi.withRoleAndCaseload("302", "MDI")
-    println(usernamesWithRole302InMdi.size)
 
+    val config = Dev()
+    val token = "TODO"
+    val prisonApi = PrisonApi(config.prisonApi, token)
+    val nomisUserRoleApi = NomisUserRoleApi(config.nomisUserRoleApi, token)
+    val nomisRole = "302"
+    val dpsRole = "PRISON_RECEPTION"
 
-//    val config: Config = Dev()
+    val prisonIds = prisonApi.findAgenciesWithType("INST")
+    println("total prisons to migrate: ${prisonIds.size}")
+    println("prisons to migrate: $prisonIds. Total: ${prisonIds.size}")
 
-//    val archiveSubDirectory = "archive"
-//    val existingSuccessfulMigrationFileName = "SUCCESSFUL_MIGRATIONS.txt"
-//    val recallMovementReasonCode = "Z"
-//    val recallImprisonmentStatus = "RECEP_HOS"
-//    val recallIsYouthOffender = false
-//    val dischargeToHospitalCommentText = "Released to hospital (Restricted Patients migration)"
-//
-//    val successfulOffenderMigrations = SuccessfulOffenderMigrations(config.resultsBaseDirectory, existingSuccessfulMigrationFileName)
-//    val progressStream = PerOffenderFileOutput(config.resultsBaseDirectory, archiveSubDirectory)
-//    val prisonApi = NomisUserRoleApi(config.prisonApiRootUrl, config.token)
-//    val rpApi = RestrictedPatientsApi(config.restrictedPatientsApiRootUrl, config.token)
-//
-//    val migrationCommand = Migrations(
-//        SpreadsheetReader(config.spreadsheetFileName),
-//        successfulOffenderMigrations,
-//        SynchronisedSummaryOutput(config.resultsBaseDirectory),
-//        PerOffenderFileOutput(config.resultsBaseDirectory, archiveSubDirectory),
-//        MigrateOffender(successfulOffenderMigrations, progressStream, prisonApi, rpApi, config.removingExistingRestrictedPatient,
-//            recallMovementReasonCode, recallImprisonmentStatus, recallIsYouthOffender, dischargeToHospitalCommentText)
-//    )
-//
-//    migrationCommand.handle(MigrationRequestEvent(11, 4))
+    val usernamesAlreadyDpsNewRole = nomisUserRoleApi.withDpsRole(dpsRole)
+    println("total users already with dps role: ${usernamesAlreadyDpsNewRole.size}")
+    println("users already with dps role: ${usernamesAlreadyDpsNewRole}")
+
+    var processedUsernames = setOf<String>()
+    for (prisonId in prisonIds) {
+        // TODO remove if statement for all prisons.
+        if (prisonId == "ALI") {
+            println("Finding users with nomis role: $nomisRole for prison $prisonId")
+            val usernamesWithRole302InPrison = nomisUserRoleApi.withRoleAndCaseload("302", prisonId)
+            println("total users with nomis role $nomisRole in prison: $prisonId. ${usernamesWithRole302InPrison.size}")
+            println("users with nomis role $nomisRole in prison: $prisonId. $usernamesWithRole302InPrison")
+
+            val usersNotYetProcessed = usernamesWithRole302InPrison.minus(processedUsernames)
+            println("users not yet processed with nomis role $nomisRole in prison: $prisonId. ${usersNotYetProcessed}")
+            println("total users not yet processed with nomis role $nomisRole in prison: $prisonId. ${usersNotYetProcessed.size}")
+
+            val usersNotYetProcessedWithoutDpsRole = usersNotYetProcessed.minus(usernamesAlreadyDpsNewRole)
+            println("users not yet processed with nomis role $nomisRole but not dps role in prison: $prisonId. ${usersNotYetProcessedWithoutDpsRole}")
+            println("total users not yet processed with nomis role $nomisRole but not dps role in prison: $prisonId. ${usersNotYetProcessedWithoutDpsRole.size}")
+
+            for (username: String in usersNotYetProcessedWithoutDpsRole) {
+                nomisUserRoleApi.giveDpsRole(dpsRole, username)
+            }
+
+            processedUsernames = processedUsernames.union(usersNotYetProcessedWithoutDpsRole)
+            println("TOTAL SO FAR: ${processedUsernames.size}")
+        }
+    }
+}
+
+class Dev {
+    val prisonApi = "https://api-dev.prison.service.justice.gov.uk"
+    val nomisUserRoleApi = "https://nomis-user-dev.aks-dev-1.studio-hosting.service.justice.gov.uk"
+}
+
+class PreProd {
+    val prisonApi = "https://api-preprod.prison.service.justice.gov.uk"
+    val nomisUserRoleApi = "https://nomis-user-pp.aks-live-1.studio-hosting.service.justice.gov.uk"
+}
+
+class Prod {
+    val prisonApi = "https://api.prison.service.justice.gov.uk"
+    val nomisUserRoleApi = "https://nomis-user.aks-live-1.studio-hosting.service.justice.gov.uk"
 }
--- src/main/kotlin/gateways/NomisUserRoleApi.kt
+++ src/main/kotlin/gateways/NomisUserRoleApi.kt
@@ -12,19 +12,70 @@ class NomisUserRoleApi (
 ) {
     private val pageSize = 100;
 
+
     @Throws(exceptionClasses = [WebClientException::class, ServerException::class])
-    @Synchronized fun withRoleAndCaseload(role: String, caseload: String) : List<String> {
-        println("Attempting to find users with role: $role for caseLoad: $caseload")
+    @Synchronized fun giveDpsRole(role: String, username: String): User {
+        println("Adding dps role: $role to user: $username")
+        val conn = URL("$nomisUserRoleApi/users/$username/roles/$role").openConnection() as HttpURLConnection
+        conn.requestMethod = "POST"
+        conn.doOutput = true
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.setRequestProperty("Authorization", "Bearer $token")
+        try {
+            val lines = conn.inputStream.use {
+                it.bufferedReader().readLines()
+            }
+            val combinedResult = lines.joinToString (" ")
+            return Json {ignoreUnknownKeys = true} .decodeFromString(combinedResult)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            val errorString = getError(conn)
+            println("Error response: ${conn.responseCode}: ${conn.responseMessage}")
+            if (conn.responseCode >= 500) {
+                throw ServerException()
+            } else if (conn.responseCode == 409) {
+                println("Username: $username already has the dps role: $role")
+                return User(username)
+            }
+            throw WebClientException(conn.responseCode, errorString)
+        } finally {
+            conn.disconnect()
+        }
+    }
+
+    @Throws(exceptionClasses = [WebClientException::class, ServerException::class])
+    @Synchronized fun withDpsRole(role: String) : Set<String> {
+        println("Attempting to find users with DPS role: $role")
+
+        val firstPage = withDpsRolePaged(role, 0, this.pageSize)
+        val totalPages = firstPage.totalPages
+        return (0 until totalPages).flatMap { withDpsRolePaged(role, it, this.pageSize).content }.map { it.username }.toSet()
+    }
+
+
+    @Throws(exceptionClasses = [WebClientException::class, ServerException::class])
+    @Synchronized fun withRoleAndCaseload(role: String, caseload: String) : Set<String> {
+        println("Attempting to find users with role: $role in caseload: $caseload")
 
         val firstPage = withRoleAndCaseloadPaged(role, caseload, 0, this.pageSize)
         val totalPages = firstPage.totalPages
-        return (0 until totalPages).flatMap { withRoleAndCaseloadPaged(role, caseload, it, this.pageSize).content }.map { it.username }.distinct()
+        return (0 until totalPages).flatMap { withRoleAndCaseloadPaged(role, caseload, it, this.pageSize).content }.map { it.username }.toSet()
     }
 
-    private fun withRoleAndCaseloadPaged(role: String, caseload: String, page: Int = 0, size: Int): Page {
-        println("Attempting to find users with role: $role for caseLoad: $caseload. Page: $page and Size: $size")
+    private fun withDpsRolePaged(role: String, page: Int, size: Int): Page {
+        println("Attempting to find users with DPS role: $role. Page: $page and Size: $size")
+        val results = withUrl("$nomisUserRoleApi/users?accessRoles=$role&status=ACTIVE&size=$size&page=$page")
+        return results
+    }
 
-        val conn = URL("$nomisUserRoleApi/users?nomisRole=$role&caseload=$caseload&status=ACTIVE&size=$size&page=$page").openConnection() as HttpURLConnection
+    private fun withRoleAndCaseloadPaged(role: String, caseload: String, page: Int, size: Int): Page {
+        println("Attempting to find users with role: $role in caseload: $caseload. Page: $page and Size: $size")
+        val results = withUrl("$nomisUserRoleApi/users?nomisRole=$role&caseload=$caseload&status=ACTIVE&size=$size&page=$page")
+        return results
+    }
+
+    private fun withUrl(url: String): Page {
+        val conn = URL(url).openConnection() as HttpURLConnection
         conn.requestMethod = "GET"
         conn.doOutput = true
         conn.setRequestProperty("Content-Type", "application/json")
